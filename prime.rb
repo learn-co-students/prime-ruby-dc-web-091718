4bdd2c4fd4ba7456f220d109f6bbf0f2a09101c2
github username <github email address>
2018-08-28 21:24:37 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+
+
+def prime?(number)
+  if number < 2
+    false
+  else
+    array = (2..number-1).to_a
+    array.each do |i|
+      x = number % i
+      if x == 0
+        return false
+      end
+    end
+    return true
+  end
+end
+
+  # number > 1 and
+  # cant be formed by multiplying 2 smaller numbers
+  # except for the number times 1
