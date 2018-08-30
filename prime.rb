59b512186f3fa7395e3edff03d75140e4f23c2db
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-30 22:00:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  else
+    possible_factors = (2..(number-2/2))
+    possible_factors.to_a.all? do |factor|
+      number%factor != 0
+    end
+  end
+end
+
